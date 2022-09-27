d25af4460c0cfd981e98a8dacf3122efbca6df45
spencerBergamo@gmail.com <spencerbergamo@gmail.com>
2022-09-27 13:27:06 -0500

    changes to tip pool files

--- JasminTesting/tip-pool/servers.test.js
+++ JasminTesting/tip-pool/servers.test.js
@@ -11,6 +11,23 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should not add a new server on submitServerInfo() with empty inputs', function() {
+    serverNameInput.value = '';
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  })
+
+  it('should update servertable on updateServerTable()', function() {
+    submitServerInfo();
+    updateServerTable();
+
+    let list = document.querySelectorAll('#serverTablle tbody tr td');
+    expect(list[0].innerText).toEqual('Alice');
+    expect(list[1].innerText).toEqual('$0.00');
+    expect(list[2].innerText).toEqual('X');
+  });
+
   afterEach(function() {
     // teardown logic
   });
