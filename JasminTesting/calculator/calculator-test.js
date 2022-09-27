77535078e78c62ca2dac043eaca2ca05abbed469
spencerBergamo@gmail.com <spencerbergamo@gmail.com>
2022-09-27 13:14:37 -0500

    changes to javascript and html files

--- JasminTesting/calculator/calculator-test.js
+++ JasminTesting/calculator/calculator-test.js
@@ -1,11 +1,35 @@
-
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {
+    amount: 10000,
+    years: 8,
+    rate: 5.8
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('130.44');
 });
 
-
-it("should return a result with 2 decimal places", function() {
-  // ..
+it("should return a result with 2 decimal places", function () {
+  const values = {
+    amount: 10043,
+    years: 8,
+    rate: 5.8
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('131.00');
 });
 
-/// etc
+it("should handle really high interest rates", function () {
+  const values = {
+    amount: 1000,
+    years: 40,
+    rate: 99
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('82.50');
+})
+
+it("should check to see if input is typeof Number", function() {
+  const values = {
+    amount: "1000",
+    years: 40,
+    rate: 99
+  };
+  expect(calculateMonthlyPayment(values)).toEqual(jasmine.any(Number));
+})
